9b3b458e6fd22f010755fcb9762404b8fc7dfb82
Elena <inculet98@mail.ru>
2023-09-25 16:18:21 +0300

    adaugare modificari

---
+++ index.js
@@ -0,0 +1,116 @@
+// const str = 'un string';
+// const number = 9;
+// const bool = true;
+// const symb = Symbol('foo');
+// const bigInt = BigInt(99999999999999999);
+// const unde = undefined
+// const nullishValue = null
+
+// const obk = {};
+// const arr = {};
+
+// console.log(typeof BigInt)
+
+// const str1 = 'Hello'
+// const str2 = 'World'
+
+// console.log(str1 + str2)
+
+// var obj = {
+//   name: 'Inculet',
+//   prenume: 'Elena',
+//   hobby: 'dance'
+// }
+
+// obj = 5
+
+// console.log(obj)
+
+// const camelCase = 'camelCase';
+// const USER_LIMIT_PER_PAGE = 'user snake case'
+// const test = new Array
+
+// const str1 = 'Hello'
+// const str2 = 'World'
+
+// console.log("${str1} ${str2} cgcbfvsfv CHFKSDDCFFVB CHF VGCFJD${str1}")
+
+
+// const condition = true;
+// const falsy1 = 0;
+// const falsy2 = '';
+// const falsy3 = undefined;
+// const falsy4 = null;
+// const falsy5 = NaN
+
+// if (falsy5) {
+//   console.log('pass')
+// } else {
+//   console.log('do not pass')
+// }
+
+
+// const variabila = 3
+
+// console.log(Boolean(NaN))
+
+
+// console.log(2 == '2')
+// console.log(2 === '2')
+
+
+// const obj = {
+//   nume: 'text',
+//   prenume: undefined
+// }
+// console.log('obj from BE', obj)
+// if (!obj.prenume) {
+//   obj.prenume = 'prenume'
+// }
+// console.log(obj, 2)
+
+// console.log(1 === 1 && 1 == 2);
+
+// const sum = 2 ** 3;
+
+// console.log(sum)
+
+// const sum = 2 ** 3;
+// console.log(Math.pow(2, 3))
+
+
+
+// const condition1 = ''
+// const condition2 = 3
+// if (!condition1 && condition2) {
+//   console.log('pass')
+// }
+
+// const num = '3'
+
+// console.log(NaN == NaN)
+
+
+// var x = 5;
+// if (true) {
+//   var x = 6
+
+//   console.log('inside block', x)
+// }
+// console.log('outside block', x)
+
+var x = 5;
+function fn() {
+  var x = 6
+  console.log(x)
+}
+fn()
+console.log('outside block', x)
+
+var x = 5;
+function fn() {
+  var x = 6
+  console.log(x)
+}
+fn()
+console.log('outside block', x)
